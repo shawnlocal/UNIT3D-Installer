90a64e6ebe0b908878f6070ffc28e1cd47c3b83e
stivi05 <49834427+stivi05@users.noreply.github.com>
2026-03-19 11:17:26 +0200

    Enhance Unit3dSetup with detailed messages and commands

--- src/Installer/UNIT3D/Unit3dSetup.php
+++ src/Installer/UNIT3D/Unit3dSetup.php
@@ -17,12 +17,11 @@ class Unit3dSetup extends BaseInstaller
         $this->crons();
 
         $this->setup();
-
     }
 
     protected function clone()
     {
-        $this->io->writeln('<fg=blue>Cloning Source Files</>');
+        $this->io->writeln('<fg=blue>Cloning Source Files from your Repository</>');
         $this->seperator();
 
         $install_dir = $this->config->os('install_dir');
@@ -32,16 +31,17 @@ class Unit3dSetup extends BaseInstaller
             $this->process(["rm -rf $install_dir"]);
         }
 
+        // Клонираме директно в инсталационната папка
         $this->process(["git clone $url $install_dir"]);
 
         if (!is_dir($install_dir)) {
-            $this->throwError('Something went wrong with the cloning process. Please report this bug!');
+            $this->throwError('Something went wrong with the cloning process. Check your repository URL!');
         }
     }
 
     protected function env()
     {
-        $this->io->writeln("\n\n<fg=blue>Preparing the '.env' File</>");
+        $this->io->writeln("\n\n<fg=blue>Preparing the '.env' File for v9.2.0</>");
         $this->seperator();
 
         $install_dir = $this->config->os('install_dir');
@@ -52,109 +52,96 @@ class Unit3dSetup extends BaseInstaller
 
         $this->createFromStub(
             [
-                '{{PROTOCOL}}' => $this->config->app('ssl') == 'yes' ? 'https' : 'http',
-                '{{FQDN}}' => $this->config->app('hostname'),
-                '{{DBDRIVER}}' => strtolower($this->config->app('database_driver')),
-                '{{DB}}' => $this->config->app('db'),
-                '{{DBUSER}}' => $this->config->app('dbuser'),
-                '{{DBPASS}}' => $this->config->app('dbpass'),
-                '{{OWNER}}' => $this->config->app('owner'),
-                '{{OWNEREMAIL}}' => $this->config->app('owner_email'),
+                '{{PROTOCOL}}'      => $this->config->app('ssl') == 'yes' ? 'https' : 'http',
+                '{{FQDN}}'          => $this->config->app('hostname'),
+                '{{DBDRIVER}}'      => strtolower($this->config->app('database_driver')),
+                '{{DB}}'            => $this->config->app('db'),
+                '{{DBUSER}}'        => $this->config->app('dbuser'),
+                '{{DBPASS}}'        => $this->config->app('dbpass'),
+                '{{OWNER}}'         => $this->config->app('owner'),
+                '{{OWNEREMAIL}}'    => $this->config->app('owner_email'),
                 '{{OWNERPASSWORD}}' => $this->config->app('password'),
-                '{{TMDBAPIKEY}}' => $this->config->app('tmdb-key'),
-                '{{MAILDRIVER}}' => $this->config->app('mail_driver'),
-                '{{MAILHOST}}' => $this->config->app('mail_host'),
-                '{{MAILPORT}}' => $this->config->app('mail_port'),
-                '{{MAILUSERNAME}}' => $this->config->app('mail_username'),
-                '{{MAILPASSWORD}}' => $this->config->app('mail_password'),
-                '{{MAILFROMNAME}}' => $this->config->app('mail_from_name')
+                '{{TMDBAPIKEY}}'    => $this->config->app('tmdb-key'),
+                '{{MAILDRIVER}}'    => $this->config->app('mail_driver'),
+                '{{MAILHOST}}'      => $this->config->app('mail_host'),
+                '{{MAILPORT}}'      => $this->config->app('mail_port'),
+                '{{MAILUSERNAME}}'  => $this->config->app('mail_username'),
+                '{{MAILPASSWORD}}'  => $this->config->app('mail_password'),
+                '{{MAILFROMNAME}}'  => $this->config->app('mail_from_name'),
+                // ДОБАВЯМЕ MEILISEARCH ТУК
+                '{{MEILI_HOST}}'    => $this->config->app('meilisearch_host'),
+                '{{MEILI_KEY}}'     => $this->config->app('meilisearch_key'),
             ],
             '../.env.stub',
             "$install_dir/.env"
         );
 
-        $this->io->writeln('<fg=green>OK</>');
+        $this->io->writeln('<fg=green>OK: .env file created with Meilisearch support.</>');
     }
 
     protected function perms()
     {
-        $this->io->writeln("\n<fg=blue>Setting Permissions</>");
+        $this->io->writeln("\n<fg=blue>Setting Proper Permissions</>");
         $this->seperator();
 
         $install_dir = $this->config->os('install_dir');
         $web_user = $this->config->os('web-user');
 
         $this->process([
-            "chown -R $web_user:$web_user /etc/letsencrypt",
-            "chown -R $web_user:$web_user " . dirname($install_dir),
-            "find $install_dir -type d -exec chmod 0775 '{}' + -or -type f -exec chmod 0664 '{}' +",
+            "chown -R $web_user:$web_user " . $install_dir,
+            "find $install_dir -type d -exec chmod 0775 '{}' +",
+            "find $install_dir -type f -exec chmod 0664 '{}' +",
             "chmod 750 $install_dir/artisan",
             "chmod 640 $install_dir/.env"
+        ]);
+        
+        // Важно за Laravel: storage и cache трябва да са writable
+        $this->process([
+            "chmod -R 775 $install_dir/storage",
+            "chmod -R 775 $install_dir/bootstrap/cache"
         ]);
     }
 
     protected function setup()
     {
-        $this->io->writeln("\n\n<fg=blue>Setting Up Web Site</>");
+        $this->io->writeln("\n\n<fg=blue>Running Core Installation Commands (This may take a while)</>");
         $this->seperator();
 
         $install_dir = $this->config->os('install_dir');
-        $fqdn = $this->config->app('hostname');
         $web_user = $this->config->os('web-user');
-        $echo_port = $this->config->app('echo-port');
-        $protocol = $this->config->app('ssl') == 'yes' ? 'https' : 'http';
 
-        $this->createFromStub([
-            '{{FQDN}}' => $fqdn,
-            '{{PORT}}' => $echo_port,
-            '{{PROTOCOL}}' => $protocol,
-        ], '../laravel-echo-server.stub', '/var/www/html/laravel-echo-server.json');
-        
-        $this->process([
-            "chown -R $web_user:$web_user $install_dir/laravel-echo-server.json",
-        ]);
-
-        $this->createFromStub([
-            '{{INSTALLDIR}}' => $install_dir,
-            '{{WEBUSER}}' => $web_user,
-        ], 'supervisor/app.conf', '/etc/supervisor/conf.d/unit3d.conf');
-
-        $this->process([
-            'supervisorctl reread',
-            'supervisorctl update',
-            'supervisorctl reload'
-        ]);
-
+        // Списък с команди за изпълнение
         $www_cmds = [
-            'laravel-echo-server client:add',
-            'composer install -q',
+            'composer install --no-interaction --optimize-autoloader',
             'bun install',
             'bun run build',
-            'php artisan key:generate',
-            'php artisan migrate --seed',
-            'php artisan auto:email-blacklist-update',
-            'php artisan test:email'
+            'php artisan key:generate --force',
+            'php artisan migrate --seed --force',
+            'php artisan scout:sync', // Синхронизиране на Meilisearch
+            'php artisan storage:link'
         ];
 
         foreach ($www_cmds as $cmd) {
+            $this->io->writeln("<fg=yellow>Executing: $cmd</>");
             $this->process([
                 "su $web_user -s /bin/bash --command=\"cd $install_dir && $cmd\""
             ], true);
         }
 
-        $this->io->writeln(' ');
+        $this->io->writeln('<fg=green>Web Site Setup Completed!</>');
     }
 
     protected function crons()
     {
-        $this->io->writeln("\n\n<fg=blue>Setting Up Crontabs</>");
+        $this->io->writeln("\n\n<fg=blue>Setting Up Laravel Scheduler</>");
         $this->seperator();
 
         $install_dir = $this->config->os('install_dir');
+        $web_user = $this->config->os('web-user');
 
+        // Добавяме крона директно към www-data потребителя
         $this->process([
-            "(crontab -l ; echo \"* * * * * php $install_dir/artisan schedule:run >> /dev/null 2>&1\") | crontab -"
+            "echo \"* * * * * php $install_dir/artisan schedule:run >> /dev/null 2>&1\" | crontab -u $web_user -"
         ]);
     }
-
 }
