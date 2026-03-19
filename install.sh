e327f716f2cde0e35c0a379fdccbe722a9b09df9
stivi05 <49834427+stivi05@users.noreply.github.com>
2026-03-19 11:09:49 +0200

    Enhance install.sh with OS check and user prompts
    Added OS detection and error handling in Bulgarian.

--- install.sh
+++ install.sh
@@ -1,22 +1,30 @@
 #!/usr/bin/env bash
 
+# Включваме цветовете за по-добра визуализация
 source tools/colors.sh
 
-# Detect OS
+# 1. Проверка за root права - задължително за инсталация на пакети
+if [[ $EUID -ne 0 ]]; then
+   echo -e "\n$Red [ГРЕШКА] Този скрипт ТРЯБВА да се изпълни с sudo или като root! $Color_Off"
+   exit 1
+fi
+
+echo -e "${BCyan}==============================================${Color_Off}"
+echo -e "${BWhite}    UNIT3D Community Installer (v9.2.0)      ${Color_Off}"
+echo -e "${BCyan}==============================================${Color_Off}"
+
+# Автоматично засичане на операционната система
 case $(head -n1 /etc/issue | cut -f 1 -d ' ') in
-    Ubuntu)     type="ubuntu" ;;
-    *)          type='' ;;
+    Ubuntu)   type="ubuntu" ;;
+    *)        type='' ;;
 esac
 
-# Unable to detect OS Properly
-# Note: OVH and other providers remove the contents of /etc/issue in their OS templates
-#   so we need to ask the user manually to tell us what the OS is as a Fallback
-# Ref: https://github.com/ServNX/UNIT3D-INSTALLER/issues/8
+# Fallback механизъм, ако автоматичното засичане се провали (често при OVH/VPS шаблони)
 if [ "$type" = '' ]; then
-    echo -e "\n$Red We was unable to automatically determine your OS! $Color_Off"
-    echo -e "\n$Purple This can happen if you are using an OS template from a provider like OVH amongst others. $Color_Off\n"
+    echo -e "\n$Red Не успяхме да определим автоматично вашата операционна система! $Color_Off"
+    echo -e "\n$Purple Това се случва при някои облачни доставчици. Моля, изберете ръчно: $Color_Off\n"
 
-    PS3='Please select the # for your OS: '
+    PS3='Моля, изберете номер за вашата ОС: '
     options=("Ubuntu 24.04" "Ubuntu 22.04" "Ubuntu 20.04" "Quit")
     select opt in "${options[@]}"
     do
@@ -37,15 +45,21 @@ if [ "$type" = '' ]; then
                 break
                 ;;
             "Quit")
+                echo -e "$Yellow Инсталацията е прекратена. $Color_Off"
                 exit 0
                 ;;
             *)
-                echo -e "$Red Invalid Option $REPLY $Color_Off"
+                echo -e "$Red Невалидна опция $REPLY $Color_Off"
                 ;;
         esac
     done
 fi
 
-if [ -e $type.sh ]; then
-    bash ./$type.sh
+# Проверка дали съществува съответния скрипт (ubuntu.sh) и стартирането му
+if [ -e "$type.sh" ]; then
+    echo -e "\n$BGreen Проверката премина успешно! Стартиране на $type.sh... $Color_Off"
+    bash "./$type.sh"
+else
+    echo -e "\n$Red [ГРЕШКА] Файлът $type.sh не беше намерен в текущата директория! $Color_Off"
+    exit 1
 fi
